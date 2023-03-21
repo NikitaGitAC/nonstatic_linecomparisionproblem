fc3b27480b0fdedec96e4b3467feb59a561834bb
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-21 16:14:34 +0530

    line comparision problem

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace non_staticlinecomparisionproblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            callenghtuc1 obj = new callenghtuc1();
+            obj.check();
+
+        }
+    }
+}
